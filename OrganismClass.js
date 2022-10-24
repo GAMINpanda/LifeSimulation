73d2f5173a88f4f6b94ba4f53bed637e0efc9dc5
Alexander Mitchiner <mitchinerfamily1@gmail.com>
2022-10-24 11:39:00 +0100

    Hunger System is up and running

--- OrganismClass.js
+++ OrganismClass.js
@@ -18,15 +18,35 @@ export class organism{ //logic for class organism
 
         this.size = Math.trunc(avg * 5) + 1 //finds size of an organism (higher stats = larger)
         this.life = Math.trunc(100 - (avg * 100)) + 1 //maximum lifespan is 100 seconds (lower stats = live longer)
-        this.hungertime = 1/avg //longer hunger time the lower the stats (less need to eat)
+        this.hungertime = (1/avg)//longer hunger time the lower the stats (less need to eat)
+        this.isEaten = false
     }
 
     NewCoords(x, y){ //updates x & y position -- makes sure to keep within canvas
+        var tempx = this.x
+        var tempy = this.y
         if (x < 100 && x> 0){
             this.x = x
         }
         if (y < 100 && y > 0){
             this.y = y
         }
+        this.HungerCalc()
+    }
+
+    HungerCalc(){
+        this.hungertime = this.hungertime - 0.01 //removes hunger every 'tick'
+
+        if (this.hungertime <= 0){
+            this.rgb = "#525252"//nice grey - colour considered 'food' and can be eaten
+        }
+    }
+
+    Replenished(deadSize){
+        this.hungertime = this.hungertime + (deadSize * (this.size/5))
+    }
+
+    OrganismHasBeenEaten(){
+        this.isEaten = true
     }
 }
--- logic.js
+++ logic.js
@@ -34,8 +34,6 @@ function create(number, rgb){
         var a = (i*5) + proceduraloffset
         var b = (i*5) + proceduraloffset
 
-        console.log("originalx: ",a,"originaly: ",b)
-
         group.push(new organism(rgb, a, b))
         organisms.push(group[i])
     }
@@ -56,26 +54,49 @@ function update(){ //updates frame
     draw(0,0, "black", 100)
     
     for(let i=0; i<organisms.length; i++){
-        var valuesArray = findOrganism((organisms[i].aggro * 10) + 5, organisms[i].x, organisms[i].y)
-        //returns in form 'return [x, y, output]'
+        if (organisms[i].rgb != "#525252"){ //colour considered food - stops moving
+            var valuesArray = findOrganism((organisms[i].aggro * 10) + 5, organisms[i].x, organisms[i].y)
+            //returns in form 'return [x, y, colour]'
 
-        if (valuesArray[0] != 0 && valuesArray[1] != 0){ //&& valuesArray[2] != organisms[i].rgb <== add to prevent movement towards same colour
-            var valuesArray2 = IterateTowardsOrganism(valuesArray[0], valuesArray[1], organisms[i])
+            if(valuesArray[2] == "#525252"){//found a dead pixel
+                for(let j=0; j<organisms.length; j++){ //linear search to figure out the organism that is dead
+                    if (organisms[j].rgb == "#525252" && organisms[j].isEaten == false){
+                        if (organisms[j].x >= valuesArray[0] && organisms[j].x <= valuesArray[0] + organisms[j].size){
+                            if (organisms[j].y >= valuesArray[1] && organisms[j].y <= valuesArray[1] + organisms[j].size){ //checks coords and colour
+                                //if here organisms[j] is probably the dead pixel
+                                console.log("An organism has been eaten")
+                                organisms[j].OrganismHasBeenEaten()
+                                organisms[i].Replenished(organisms[j].size)
+                                console.log("New hunger is: ",organisms[i].hungertime)
+                            }
+                        }
+                    }
+                }
+            }
 
-            organisms[i].NewCoords(valuesArray2[0], valuesArray2[1])
+            if (valuesArray[0] != 0 && valuesArray[1] != 0){ //&& valuesArray[2] != organisms[i].rgb <== add to prevent movement towards same colour
+                var valuesArray2 = IterateTowardsOrganism(valuesArray[0], valuesArray[1], organisms[i])
+
+                organisms[i].NewCoords(valuesArray2[0], valuesArray2[1])
+            }
+            
+            else{//moves a random amount
+                organisms[i].NewCoords(organisms[i].x + Math.trunc((Math.random() - 0.5) * 5), organisms[i].y + Math.trunc((Math.random() - 0.5) * 5))
+            }
+            
+            let xinput = organisms[i].x
+            let yinput = organisms[i].y
+            let colourinput = organisms[i].rgb
+            let sizeinput = organisms[i].size
+
+            //console.log("xinput: ",xinput,"yinput: ",yinput,"colourinput: ",colourinput,"sizeinput: ",sizeinput, "i: ",i)
+            draw(xinput, yinput, colourinput, sizeinput)
         }
-        
-        else{//moves a random amount
-            organisms[i].NewCoords(organisms[i].x + Math.trunc((Math.random() - 0.5) * 5), organisms[i].y + Math.trunc((Math.random() - 0.5) * 5))
+        else{
+            if (organisms[i].isEaten == false){
+                draw(organisms[i].x, organisms[i].y, "#525252", organisms[i].size)
+            }
         }
-        
-       let xinput = organisms[i].x
-       let yinput = organisms[i].y
-       let colourinput = organisms[i].rgb
-       let sizeinput = organisms[i].size
-
-        //console.log("xinput: ",xinput,"yinput: ",yinput,"colourinput: ",colourinput,"sizeinput: ",sizeinput, "i: ",i)
-        draw(xinput, yinput, colourinput, sizeinput)
     }
     requestAnimationFrame(update)
 }
